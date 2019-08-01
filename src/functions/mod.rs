e14f281417727a6c6dd9a61b9c3cc2d1983d4742
Egor Dmitriev <egordmitriev2@gmail.com>
2019-08-01 17:25:49 +0200

    Added support for p1, p2 and pk normalization Added normalization to tempogram

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -2,8 +2,10 @@ pub mod scalar;
 pub mod vec;
 pub mod conv;
 pub mod padding;
+pub mod normalization;
 
 pub use scalar::*;
 pub use vec::*;
 pub use conv::*;
 pub use padding::*;
+pub use normalization::*;
---
+++ src/functions/normalization.rs
@@ -0,0 +1,31 @@
+use litcontainers::*;
+use itertools::Itertools;
+use num_traits::pow::Pow;
+use num_traits::{Float, Signed};
+
+pub fn normalize_cols_p1_inplace<T, R, C, S>(s: &mut S)
+	where T: Scalar + Signed, R: Dim, C: Dim, S: StorageMut<T, R, C>
+{
+	normalize_cols_inplace(s, |s| norm_p1(s))
+}
+
+pub fn normalize_rows_p1_inplace<T, R, C, S>(s: &mut S)
+	where T: Scalar + Signed, R: Dim, C: Dim, S: StorageMut<T, R, C>
+{
+	normalize_rows_inplace(s, |s| norm_p1(s))
+}
+
+pub fn normalize_cols_inplace<T, R, C, S, F>(s: &mut S, norm: F)
+	where T: Scalar, R: Dim, C: Dim, S: StorageMut<T, R, C>,
+	      F: Fn(&SliceMut<T, R, S::RStride, U1, S::CStride>) -> T
+{
+	s.as_col_slice_mut_iter().for_each(|mut col| col /= norm(&col));
+}
+
+pub fn normalize_rows_inplace<T, R, C, S, F>(s: &mut S, norm: F)
+	where T: Scalar, R: Dim, C: Dim, S: StorageMut<T, R, C>,
+		  F: Fn(&SliceMut<T, U1, S::RStride, C, S::CStride>) -> T
+{
+	s.as_row_slice_mut_iter().for_each(|mut row| row /= norm(&row));
+}
+
