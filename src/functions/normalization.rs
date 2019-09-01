f61228d3d84e5386da9e44c4f853f9e07b1b7fbc
Egor Dmitriev <egordmitriev2@gmail.com>
2019-09-01 12:42:38 +0200

    Refactored litempo

--- src/functions/normalization.rs
+++ src/functions/normalization.rs
@@ -14,14 +14,14 @@ pub fn normalize_rows_p1_inplace<T, S>(s: &mut S)
 }
 
 pub fn normalize_cols_inplace<T, S, F>(s: &mut S, norm: F)
-	where T: Scalar, S: StorageMut<T>,
+	where T: NumericElement, S: StorageMut<T>,
 	      F: Fn(&SliceMut<T, S::Rows, S::RowStride, U1, S::ColStride>) -> T
 {
 	s.as_col_slice_iter_mut().for_each(|mut col| col /= norm(&col));
 }
 
 pub fn normalize_rows_inplace<T, S, F>(s: &mut S, norm: F)
-	where T: Scalar, S: StorageMut<T>,
+	where T: NumericElement, S: StorageMut<T>,
 		  F: Fn(&SliceMut<T, U1, S::RowStride, S::Cols, S::ColStride>) -> T
 {
 	s.as_row_slice_iter_mut().for_each(|mut row| row /= norm(&row));
